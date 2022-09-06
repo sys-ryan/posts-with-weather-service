c80a2341d9e575284220fedc4471ab4ec6e71c7d
Ryan Seong-Jin Kim <63445753+sys-ryan@users.noreply.github.com>
2022-09-06 16:27:28 +0900

    Revert "fix: controller id 에 ParseIntPipe 적용"
    This reverts commit ae451a87c5fdd87d6c45f00bd8eb09903421428f.

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -7,7 +7,6 @@ import {
   Param,
   Delete,
   UseGuards,
-  ParseIntPipe,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
@@ -33,23 +32,20 @@ export class PostsController {
 
   @Serialize(PostResponseDto)
   @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.postsService.findOne(id);
+  findOne(@Param('id') id: string) {
+    return this.postsService.findOne(+id);
   }
 
   @Serialize(PostResponseDto)
   @Patch(':id')
   @UseGuards(SecretPostGuard)
-  update(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() updatePostDto: UpdatePostDto,
-  ) {
-    return this.postsService.update(id, updatePostDto);
+  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+    return this.postsService.update(+id, updatePostDto);
   }
 
   @Delete(':id')
   @UseGuards(SecretPostGuard)
-  remove(@Param('id', ParseIntPipe) id: number) {
-    return this.postsService.remove(id);
+  remove(@Param('id') id: string) {
+    return this.postsService.remove(+id);
   }
 }
