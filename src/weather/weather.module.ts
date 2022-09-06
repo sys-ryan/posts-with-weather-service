5a894fb05e4ab743e6ff7d3227ba51c71140e192
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-06 22:42:26 +0900

    feat: weather 정보fetch 및 저장 기능 구현

--- src/weather/weather.module.ts
+++ src/weather/weather.module.ts
@@ -1,7 +1,20 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from 'src/posts/entities/post.entity';
+import { Weather } from './entities/weather.entity';
+import { WeatherController } from './weather.controller';
 import { WeatherService } from './weather.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Weather, Post]),
+    ConfigModule,
+    HttpModule,
+  ],
+  controllers: [WeatherController],
   providers: [WeatherService],
+  exports: [WeatherService],
 })
 export class WeatherModule {}
--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -1,4 +1,76 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import {
+  Injectable,
+  NotFoundException,
+  ServiceUnavailableException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { string } from 'joi';
+import { lastValueFrom, map } from 'rxjs';
+import { Post } from 'src/posts/entities/post.entity';
+import { Repository } from 'typeorm';
+import { Weather } from './entities/weather.entity';
+
+interface Condition {
+  text: string;
+  icon: string;
+  code: number;
+}
 
 @Injectable()
-export class WeatherService {}
+export class WeatherService {
+  constructor(
+    @InjectRepository(Weather) private weatherRepository: Repository<Weather>,
+    @InjectRepository(Post) private postRepository: Repository<Post>,
+    private configService: ConfigService,
+    private httpService: HttpService,
+  ) {}
+
+  private baseUrl = `http://api.weatherapi.com/v1/`;
+  private key = this.configService.get<string>('WEATHER_API_KEY');
+
+  /**
+   * 요청된 latitude, longitude 위치의 현재 날씨를 조회합니다.
+   * @param lat 위도
+   * @param lng 경도
+   * @returns 현재 날씾 정보
+   */
+  async fetchWeather(lat: string, lng: string) {
+    const url = `${this.baseUrl}current.json?key=${this.key}&q=${lat},${lng}&aqi=no`;
+
+    const response$ = this.httpService.get(url);
+    const data = (await lastValueFrom(response$)).data.current
+      .condition as Condition;
+
+    return data;
+  }
+
+  /**
+   * 현재 날씨 정보를 fetch하고, 게시글과 연관시켜 현재 날씨를 DB에 저장합니다.
+   * @param postId 날씨 정보를 연관시킬 게시글의 id
+   * @param lat 위도
+   * @param lng 경도
+   */
+  async saveCurrentWeather(
+    postId: number,
+    lat: string,
+    lng: string,
+  ): Promise<void> {
+    const { text, icon, code } = await this.fetchWeather(lat, lng);
+
+    const post = await this.postRepository.findOne({ where: { id: postId } });
+    if (!post) {
+      throw new NotFoundException('Post not found.');
+    }
+
+    const weather = await this.weatherRepository.create({
+      post,
+      text,
+      icon,
+      code,
+    });
+
+    await this.weatherRepository.save(weather);
+  }
+}
