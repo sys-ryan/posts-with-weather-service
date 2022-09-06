4d53526284ca17dec80b38e2dcb0f326c2b3de41
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-06 14:04:58 +0900

    feat: 게시글 생성 기능 구현

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { Post } from './entities/post.entity';
 
 @Injectable()
 export class PostsService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+  constructor(
+    @InjectRepository(Post) private postRepository: Repository<Post>,
+  ) {}
+
+  /**
+   * 새로운 게시글을 생성합니다.
+   * @param createPostDto //
+   * @returns void
+   */
+  async create(createPostDto: CreatePostDto): Promise<void> {
+    const post = await this.postRepository.create(createPostDto);
+    await this.postRepository.save(post);
+
+    return;
   }
 
   findAll() {
