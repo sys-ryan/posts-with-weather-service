de74e04c03bd8054e5c84d3f8220dcc581372968
Ryan Seong-Jin Kim <63445753+sys-ryan@users.noreply.github.com>
2022-09-06 16:27:28 +0900

    Revert "feat: 게시글 생성 기능 구현"
    This reverts commit 4d53526284ca17dec80b38e2dcb0f326c2b3de41.

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Post } from './entities/post.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
   controllers: [PostsController],
-  providers: [PostsService],
+  providers: [PostsService]
 })
 export class PostsModule {}
