dd73345b11a96c1e9789da31573ce77a9ba617f2
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-06 21:19:00 +0900

    feat: weather entity 생성 및 post 와 relation

---
+++ src/weather/entities/weather.entity.ts
@@ -0,0 +1,20 @@
+import { Post } from 'src/posts/entities/post.entity';
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Weather {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 20 })
+  text: string;
+
+  @Column()
+  icon: string;
+
+  @Column()
+  code: number;
+
+  @OneToOne(() => Post, (post) => post.weather)
+  post: Post;
+}
