2b16aabad3fc7a3b354119a8e2a09d21cd39ea3c
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-06 22:42:26 +0900

    feat: weather 정보fetch 및 저장 기능 구현

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,9 +3,10 @@ import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
+import { WeatherModule } from 'src/weather/weather.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [TypeOrmModule.forFeature([Post]), WeatherModule],
   controllers: [PostsController],
   providers: [PostsService],
 })
