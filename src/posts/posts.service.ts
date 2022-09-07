a42889eac158f725f35d3c51a6fe853d9bea985a
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-07 16:08:08 +0900

    test: Posts Service Unit Test
    - 게시글 생성 성공 검증 (날씨 정보 포함 확인)
    - 게시글 생성시 비밀번호 암호화 검증
    - 게시글 리스트 조회 성공 검증
    - 게시글 조회 성공 검증
    - 존재하지 않는 게시글 id로 조회시 Exception 검증
    - 게시글 수정 성공 검증
    - 존재하지 않는 게시글 id로 수정시 Exception 검증
    - 게시글 삭제 성공 검증
    - 존재하지 않는 게시글 id로 삭제시 Exception 검증

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -5,7 +5,7 @@ import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { Posts } from './entities/post.entity';
 import * as bcrypt from 'bcrypt';
-import { WeatherService } from 'src/weather/weather.service';
+import { WeatherService } from '../weather/weather.service';
 
 export interface PostsList {
   data: Posts[];
